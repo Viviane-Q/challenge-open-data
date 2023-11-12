6f07ca7408f2ea0a13814a036c06e6faa71621d7
Antoine Klein <antoineklein2000@gmail.com>
2023-11-12 16:25:05 +0100

    affichage de la courbe

--- curve.js
+++ curve.js
@@ -30,10 +30,6 @@ const curveTitle = document.querySelector('#curve-title');
 const curveSvg = document.querySelector('#curve-chart');
 
 
-var Gen = d3.line()
-    .x((p) => new Date(p.xpoint, 0, 1))
-    .y((p) => p.ypoint)
-    .curve(d3.curveBasis);
 
 const svg = d3.select(document.getElementById('curve-chart'))
 
@@ -54,28 +50,35 @@ function curveReady(selectedEnergyType, data) {
             consumedTT += values.consumption ? values.consumption : 0;
             reserveTT += values.reserves ? values.reserves : 0;
         }
-        consumptionPoints.push({ xpoint: year.substring(1), ypoint: consumedTT });
-        productionPoints.push({ xpoint: year.substring(1), ypoint: productedTT });
-        reservesPoints.push({ xpoint: year.substring(1), ypoint: reserveTT });
+        consumptionPoints.push({ xpoint: parseInt(year.substring(1)), ypoint: Math.round(consumedTT) });
+        productionPoints.push({ xpoint: year.substring(1), ypoint: Math.round(productedTT) });
+        reservesPoints.push({ xpoint: year.substring(1), ypoint: Math.round(reserveTT) });
     }
     console.log(consumptionPoints);
     // Add X axis --> it is a date format
     var x = d3.scaleTime()
-        .domain(d3.extent(consumptionPoints, function(d) { return new Date(d.xpoint, 0, 1); }))
+        .domain(d3.extent(consumptionPoints, function (d) { return new Date(d.xpoint, 0, 1); }))
         .range([20, width - 50]);
     svg.append("g")
-        .attr("transform", "translate(30," + height/2  + ")")
+        .attr("transform", "translate(30," + height / 2 + ")")
         .call(d3.axisBottom(x));
     // Add Y axis
     var y = d3.scaleLinear()
-        .domain([0, d3.max(consumptionPoints, function(d) { return d.ypoint; })])
-        .range([height/2, 0]);
+        .domain([0, d3.max(consumptionPoints, function (d) { return d.ypoint; })])
+        .range([height / 2, 0]);
     svg.append("g")
         .attr("transform", "translate(50,0)")
         .call(d3.axisLeft(y));
     // Add the line
 
-    svg.append("path").datum(consumptionPoints).attr("fill", "none")
+    var Gen = d3.line()
+        .x((p) => x(new Date(p.xpoint, 0, 1)))
+        .y((p) => y(p.ypoint))
+        .curve(d3.curveBasis);
+
+    svg.append("path").datum(consumptionPoints)
+        .attr("fill", "none")
+        .attr("transform", "translate(30,0)")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
         .attr("d", Gen);
